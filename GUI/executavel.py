14ff718ab263942c316108e7beb140153aeb48d8
PedroTayer <pedro.negro.tayer@gmail.com>
2021-07-15 09:22:50 -0300

    ss tela inicial

--- GUI/executavel.py
+++ GUI/executavel.py
@@ -1,5 +1,6 @@
 import os
 import sys
+# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import matplotlib
 import pandas as pd
---
+++ main_exe.py
@@ -0,0 +1,4 @@
+app = QApplication(sys.argv)  # instantiate a QtGui (holder for the app)
+window = MyApp()
+window.show()
+sys.exit(app.exec_())
